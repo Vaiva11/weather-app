5f45a6e4ce4821c57c1bd2aa4495c9a5fbd99133
Vaiva Gribauskaite <vaiva.gribauskaite@gmail.com>
2019-06-22 20:15:17 +0300

    added selector without api

--- src/app/pages/Home/index.js
+++ src/app/pages/Home/index.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import './index.scss';
+import 'react-dropdown/style.css'
+import Select from 'react-select'
 
 function Home() {
+  const countries = [
+    {
+      label: "Lithuania", value: 1
+    },
+    {
+      label: "Latvia", value: 2
+    },
+    {
+      label: "Estonia", value: 3
+    },
+    {
+      label: "Poland", value: 4
+    },
+  ]
+
   return (
-    <React.Fragment><h1>Home</h1></React.Fragment>
+    <div className="home">
+      <div className="home--list">
+        <h1>Choose country</h1>
+        <Select options={countries} className="selector" />
+      </div>
+      <div className="home--map">
+        MAP
+      </div>
+      <div className="home--cities">
+        cities
+      </div>
+    </div>
   )
 }
 
